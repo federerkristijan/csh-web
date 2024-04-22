1fb7b27de28e8604b3176049df22b95c3dc1eaaa
Kristijan Federer <federer.kristijan@gmail.com>
2024-04-22 13:08:08 +0200

    h1 and h3 fixed. need image and text-white

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center">
       <Container>
         <div className="relative">
-            <h1 className="text-4xl font-bold p-3 top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+            <h1 className="absolute border-4 border-blue-500 text-4xl font-bold p-3 bottom-12 left-32 transform -translate-x-1/2 -translate-y-1/6">
               CANNA SMOKE HERE?
             </h1>
-            <h3 className="text-2xl font-bold bottom-6 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+            <h3 className="absolute text-2xl font-bold top-48 text-nowrap">
               Your best bud in your pocket
             </h3>
         </div>
