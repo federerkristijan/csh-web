12c24ed158d2073a3f395f4a9ff55f018349f243
Kristijan Federer <federer.kristijan@gmail.com>
2024-04-22 13:43:26 +0200

    bg-image done

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center">
       <Container>
         <div className="relative">
-            <h1 className="absolute border-4 border-blue-500 text-4xl font-bold p-3 bottom-12 left-32 transform -translate-x-1/2 -translate-y-1/6">
+            <h1 className="absolute text-white border-4 border-blue-500 text-4xl font-bold p-3 bottom-12 left-32 transform -translate-x-1/2 -translate-y-1/6">
               CANNA SMOKE HERE?
             </h1>
-            <h3 className="absolute text-2xl font-bold top-48 text-nowrap">
+            <h3 className="absolute text-white text-2xl font-bold top-48 text-nowrap">
               Your best bud in your pocket
             </h3>
         </div>
