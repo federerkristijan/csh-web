12fbd8befe303123006389bfaffa4d8f160e3997
Kristijan Federer <federer.kristijan@gmail.com>
2024-05-04 22:58:43 +0200

    rendering map. need red circle around schools/kitas

---
+++ components/pages/MapComponent.tsx
@@ -0,0 +1,45 @@
+// @/components/pages/MapComponent.tsx
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Circle } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople';
+import ReactDOMServer from 'react-dom/server';
+import { FacilitiesData } from '@/types/global'; // Ensure this type is correctly defined to match the expected facility data structure
+
+// Custom marker using MUI Icon
+const createCustomMarkerIcon = () => {
+  const iconHtml = ReactDOMServer.renderToString(<EmojiPeopleIcon style={{ fontSize: '30px', color: 'blue' }} />);
+  return L.divIcon({
+    html: iconHtml,
+    iconSize: [30, 30],
+    iconAnchor: [15, 15], // Adjust as needed
+    className: '' // This removes default extra padding and border
+  });
+};
+
+interface MapComponentProps {
+  userPosition: { lat: number, lng: number };
+  facilities: FacilitiesData[];
+}
+
+const MapComponent: React.FC<MapComponentProps> = ({ userPosition, facilities }) => {
+  const userIcon = createCustomMarkerIcon();
+
+  return (
+    <MapContainer center={{ lat: userPosition.lat, lng: userPosition.lng }} zoom={13} style={{ height: '60vh', width: '70%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={userPosition} icon={userIcon}>
+        <Circle center={userPosition} radius={100} color="red" fillOpacity={0.7} />
+      </Marker>
+      {facilities.map((facility, index) => (
+        <Circle key={index} center={{ lat: facility.latitude, lng: facility.longitude }} radius={100} color="red" fillOpacity={0.7} />
+      ))}
+    </MapContainer>
+  );
+};
+
+export default MapComponent;
