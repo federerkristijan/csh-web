34cb279e40b535743b4c776015d838ae02512d55
Kristijan Federer <federer.kristijan@gmail.com>
2024-05-15 12:22:10 +0200

    fixed page layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <div className="relative min-h-[55vh]">
       <Container>
-        <div className="border-2 border-blue-500">
-          <div className="absolute right-[15%] w-min -top-4 mt-6 bg-slate-900/70">
+        <div>
+          <div className="absolute right-[50%] w-min -top-4 mt-6 bg-slate-900/70">
             <h1 className="border-3 rounded-md border-blue-500 text-5xl font-bold px-1 py-3">
               CANNA SMOKE HERE?
             </h1>
           </div>
-          <div className="absolute bottom-2 text-center w-max">
-            <h3 className="text-[1.6rem] font-extrabold">
+          <div className="absolute bottom-6 left-3 text-center w-max">
+            <h3 className="text-[1.6rem] font-extrabold ml-6">
               Your best bud in your pocket
             </h3>
           </div>
--- components/global/navbar/Navbar.tsx
+++ components/global/navbar/Navbar.tsx
@@ -12,6 +12,7 @@ const NavBar = async () => {
   bg-black
   z-30
   shadow-sm
+  inline
   "
     >
       <div className="py-4">
@@ -24,7 +25,7 @@ const NavBar = async () => {
           md:gap-0
           "
         >
-          <Link href="/check-location" className="flex flex-row items-center text-3xl gap-3 border-2 border-s-neutral-300 p-3 rounded-2xl hover:bg-blue-900 hover:scale-110">
+          <Link href="/check-location" className="flex flex-row items-center justify-center text-3xl gap-3 border-2 border-s-neutral-300 p-3 rounded-2xl hover:bg-blue-900 hover:scale-110">
             <h1>Check your</h1>
             <Image
               src={location}
