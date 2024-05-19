d539f6e9eda4e9d751563f2662e0a19f6d194dbb
Kristijan Federer <federer.kristijan@gmail.com>
2024-05-19 20:56:58 +0200

    footer done

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -13,25 +13,27 @@ export default function Footer(props: Props) {
       // TODO: url share app
       url: "/",
     },
+    // {
+    //   title: "Rate our app",
+    //   // TODO: url rate app
+    //   url: "/",
+    // },
     {
-      title: "Rate our app",
-      // TODO: url rate app
-      url: "/",
-    },
-    {
-      title: "Gov rules on Berlin.de",
+      title: "Rules on Berlin.de",
       // TODO: url gov rules
       url: "/",
     },
   ];
 
   return (
-    <div className="fixed bottom-0 w-full flex justify-center mx-auto">
-      <div className="flex gap-4 lg:text-2xl pb-12 md:text-lg text-center mx-2">
+    <div className="flex fixed bottom-0 w-full mx-auto ">
+      <div className="flex lg:text-2xl pb-4 md:text-lg mx-2 font-semibold">
         {items.map((item, index) => (
-          <a key={index} href={item.url} className="lg:text-[#C900A5]">
+          <div key={index}>
+          <a  href={item.url} className="flex lg:text-[#C900A5] mx-2 w-fit text-center">
             {item.title}
           </a>
+          </div>
         ))}
       </div>
     </div>
