e6c26429af41352d5faa44ba4618c67a91ff53b8
Kristijan Federer <federer.kristijan@gmail.com>
2024-05-20 10:28:34 +0200

    ShareModal.tsx

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+'use client';
+
+import React, { useState } from "react";
+import ShareModal from "../ui/ShareModal";
 
 type Props = {};
 
 export default function Footer(props: Props) {
+  const [isShareModalOpen, setIsShareModalOpen] = useState(false);
+
+  const handleShareClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    event.preventDefault();
+    setIsShareModalOpen(true);
+  };
+
   const items = [
     {
       title: "T&(H)Cs",
@@ -27,15 +37,29 @@ export default function Footer(props: Props) {
 
   return (
     <div className="flex fixed bottom-0 w-full justify-center">
-      <div className="flex lg:text-2xl pb-4 md:text-lg mx-2 font-semibold">
-        {items.map((item, index) => (
-          <div key={index}>
-          <a  href={item.url} className="flex lg:text-[#C900A5] mx-2 w-fit text-center">
-            {item.title}
-          </a>
-          </div>
-        ))}
-      </div>
+       <div className="flex lg:text-2xl pb-4 md:text-lg mx-2 font-semibold">
+          {items.map((item, index) => (
+            <div key={index}>
+              {item.title === "Share our app" ? (
+                <a
+                  href={item.url}
+                  className="flex lg:text-[#C900A5] mx-2 w-fit text-center"
+                  onClick={handleShareClick}
+                >
+                  {item.title}
+                </a>
+              ) : (
+                <a
+                  href={item.url}
+                  className="flex lg:text-[#C900A5] mx-2 w-fit text-center"
+                >
+                  {item.title}
+                </a>
+              )}
+            </div>
+          ))}
+        </div>
+        <ShareModal isOpen={isShareModalOpen} onClose={() => setIsShareModalOpen(false)} />
     </div>
   );
 }
