41f44daa54a8b1ba1db8c92f12e129ed3d9f668f
Kristijan Federer <94006870+federerkristijan@users.noreply.github.com>
2024-05-21 23:06:13 +0200

    Update page.tsx

--- app/check-location/page.tsx
+++ app/check-location/page.tsx
@@ -8,16 +8,22 @@ const CheckLocationClient = dynamic(() => import('@/components/pages/CheckLocati
 
 const fetchGeoJsonData = async () => {
   const geoJsonUrl = process.env.NEXT_PUBLIC_GEOJSON_URL;
+  if (!geoJsonUrl) {
+    console.error('GeoJSON URL is not defined');
+    throw new Error('GeoJSON URL is not defined');
+  }
   try {
     const response = await fetch(geoJsonUrl);
-    console.log(response.ok, 'let us hear a prayer');
+    console.log(response.ok, 'Response OK');
     if (!response.ok) {
+      console.log('Failed to fetch GeoJSON data:', response.status, response.statusText);
       throw new Error('Failed to fetch GeoJSON data');
     }
     const data = await response.json();
+    console.log('GeoJSON Data:', data);
     return data;
   } catch (error) {
-    console.error(error);
+    console.error('Error:', error);
     return null;
   }
 };
