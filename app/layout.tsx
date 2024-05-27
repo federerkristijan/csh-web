c149b634c59ae089d32320363ea838c2398b4382
Kristijan Federer <federer.kristijan@gmail.com>
2024-05-27 12:02:44 +0200

    fix the main element - grow:1 removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
               <MobileFooter />
             </div>
             <Navbar />
-            {children}
+            <main className="flex justify-center items-center">{children}</main>
             <div className="hidden lg:block md:block">
               <Footer />
             </div>
