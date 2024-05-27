56ed6972a1b37cab7ff46a7b2c7201dd0c40645b
Kristijan Federer <federer.kristijan@gmail.com>
2024-05-27 10:23:44 +0200

    allowLocation: mx-[2rem]

--- components/shared/AllowLocation.tsx
+++ components/shared/AllowLocation.tsx
@@ -56,7 +56,7 @@ const AllowLocationDialog: React.FC<AllowLocationDialogProps> = ({
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <div className="inline-block align-bottom md:w-2/6 bg-[#383838] rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-fit sm:p-6">
+            <div className="inline-block align-bottom md:w-2/6 bg-[#383838] rounded-lg px-4 pt-5 pb-4 mx-[2rem] text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-fit sm:p-6">
               <div>
                 <div className="mt-3 text-center sm:mt-5">
                   <Dialog.Title
