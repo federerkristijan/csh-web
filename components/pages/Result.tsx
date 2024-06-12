c29807096d9a1bbadfb779b3befc1cb7853a5d09
Kristijan Federer <federer.kristijan@gmail.com>
2024-06-12 14:26:26 +0200

    result typos fixed

--- components/pages/Result.tsx
+++ components/pages/Result.tsx
@@ -45,11 +45,11 @@ const Result: React.FC<ResultProps> = ({ canSmoke, closestDistance }) => {
         ) : (
           <div className="flex flex-col items-center justify-center text-white">
             <Image src={NO} alt="no" width={100} height={100} />
-            <p className="result-text text-[27px] font-bold mt-[5px]">You can NOT smoke here</p>
+            <p className="result-text text-[27px] font-bold mt-[5px]">You canna NOT smoke here</p>
             <p className="quote text-[22px] bg-red-600 rounded-xl py-[10px] px-[10px] mt-[30px]">{quote}</p>
             {closestDistance !== null && (
               <p className="distance text-[18px] mt-[10px]">
-                Closest restricted area is {Math.round(closestDistance)} meters away.
+                Closest non-restricted area is {Math.round(closestDistance)} meters away.
               </p>
             )}
           </div>
